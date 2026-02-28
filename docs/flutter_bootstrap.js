4f2b65e367b4457748edd37378edc3fc9452224e
massimo martella <massimomartella@iMac-di-massimo.local>
2026-02-28 13:51:11 +0100

    Add save anywhere feature

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1053677675" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3103237914" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
